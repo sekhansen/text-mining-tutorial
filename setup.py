41b836765560c15dd0cf93298419d1ac5d73cc51
pesoto <psoto771@gmail.com>
2014-11-23 20:40:50 +0100

    Cancel cython_gsl

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import cython_gsl
+ 
 import numpy
+import sys
+
+if sys.platform == "win32":
+	include_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\include"
+	lib_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\lib"
+else:
+	include_gsl_dir = sys.exec_prefix+"\\include"
+	lib_gsl_dir = sys.exec_prefix+"\\lib"
   
      
 ext = Extension("samplers", ["samplers.pyx"],
     include_dirs=[numpy.get_include(), 
-                  cython_gsl.get_cython_include_dir()],
-    library_dirs=[cython_gsl.get_library_dir()],
-    libraries=libraries=cython_gsl.get_libraries()
+                  include_gsl_dir],
+    library_dirs=[lib_gsl_dir],
+    libraries=["gsl","gslcblas","m"]
 )
  
 setup(name = "samplers",
 	ext_modules=[ext],
-    cmdclass = {'build_ext': build_ext}
-	include_dirs = [cython_gsl.get_include())
+    cmdclass = {'build_ext': build_ext})
