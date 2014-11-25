cfc630bb8cafb10b6de64a25c7a55237765c392f
pesoto <psoto771@gmail.com>
2014-11-25 12:38:17 +0100

    Update setup.py
    Adjusted Setup.py to exclude GSL changes

--- setup.py
+++ setup.py
@@ -5,19 +5,24 @@ from Cython.Distutils import build_ext
 import numpy
 import sys
 
-if sys.platform == "win32":
-	include_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\include"
-	lib_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\lib"
-else:
-	include_gsl_dir = sys.exec_prefix+"\\include"
-	lib_gsl_dir = sys.exec_prefix+"\\lib"
+#Se GSL Library
+#if sys.platform == "win32":
+#	include_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\include"
+#	lib_gsl_dir = sys.exec_prefix.lower().split("anaconda")[0]+"anaconda\\gsl\\lib"
+#else:
+#	include_gsl_dir = sys.exec_prefix+"\\include"
+#	lib_gsl_dir = sys.exec_prefix+"\\lib"
   
      
+#ext = Extension("samplers", ["samplers.pyx"],
+#    include_dirs=[numpy.get_include(), 
+#                  include_gsl_dir],
+#    library_dirs=[lib_gsl_dir],
+#    libraries=["gsl","gslcblas","m"]
+#)
+
 ext = Extension("samplers", ["samplers.pyx"],
-    include_dirs=[numpy.get_include(), 
-                  include_gsl_dir],
-    library_dirs=[lib_gsl_dir],
-    libraries=["gsl","gslcblas","m"]
+    include_dirs=[numpy.get_include()]
 )
  
 setup(name = "samplers",
